4b58cf3cae75bd79283b3415891552f5e975cb55
Eric Gregory <mrericsir@gmail.com>
2013-08-30 22:57:20 -0700

    Touch improvements!

--- src/models/MouseWheelArea.cpp
+++ src/models/MouseWheelArea.cpp
@@ -3,6 +3,8 @@
 MouseWheelArea::MouseWheelArea(QDeclarativeItem *parent) :
     QDeclarativeItem(parent)
 {
+    // Let touchpad events go through.
+    setAcceptTouchEvents(true);
 }
 
 MouseWheelArea::~MouseWheelArea()
