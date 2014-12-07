488d463f4da5d4ba60894259626cc9ce383e3b60
MrEricSir <mrericsir@gmail.com>
2014-12-06 21:49:00 -0800

    "Vacuum" database to reclaim lost disk space when expiring news

--- src/operations/ExpireNewsOperation.cpp
+++ src/operations/ExpireNewsOperation.cpp
@@ -14,35 +14,51 @@ ExpireNewsOperation::ExpireNewsOperation(OperationManager *parent, ListModel *fe
 
 void ExpireNewsOperation::execute()
 {
-    db().transaction();
+    bool performVacuum = false;
 
-    // SQL can't easily do loops, so we need a little C++ magic.
-    for (int i = 0; i < feedList->rowCount(); i++) {
-        FeedItem* feed = qobject_cast<FeedItem*>(feedList->row(i));
-        if (feed->isSpecialFeed()) {
-            // Can't delete a special feed, DUH.
-            continue;
+    {
+        db().transaction();
+
+        // SQL can't easily do loops, so we need a little C++ magic.
+        for (int i = 0; i < feedList->rowCount(); i++) {
+            FeedItem* feed = qobject_cast<FeedItem*>(feedList->row(i));
+            if (feed->isSpecialFeed()) {
+                // Can't delete a special feed, DUH.
+                continue;
+            }
+
+            QSqlQuery query(db());
+            query.prepare("DELETE FROM NewsItemTable WHERE feed_id = :feed_id AND NOT pinned "
+                          "AND id < ( SELECT bookmark_id FROM FeedItemTable WHERE id = :feed_id2 LIMIT 1 ) "
+                          "AND timestamp < :olderThan AND id NOT IN "
+                          "( SELECT id FROM NewsItemTable WHERE feed_id = :feed_id3 ORDER BY timestamp DESC LIMIT :saveLast )");
+            query.bindValue(":feed_id", feed->getDbId());
+            query.bindValue(":feed_id2", feed->getDbId());
+            query.bindValue(":olderThan", olderThan.toMSecsSinceEpoch());
+            query.bindValue(":feed_id3", feed->getDbId());
+            query.bindValue(":saveLast", saveLast);
+
+            if (!query.exec()) {
+                reportSQLError(query, "Unable to remove old feed.");
+                db().rollback();
+
+                return;
+            }
+
+            if (query.numRowsAffected() > 0) {
+                performVacuum = true;
+            }
         }
 
-        QSqlQuery query(db());
-        query.prepare("DELETE FROM NewsItemTable WHERE feed_id = :feed_id AND NOT pinned "
-                      "AND id < ( SELECT bookmark_id FROM FeedItemTable WHERE id = :feed_id2 LIMIT 1 ) "
-                      "AND timestamp < :olderThan AND id NOT IN "
-                      "( SELECT id FROM NewsItemTable WHERE feed_id = :feed_id3 ORDER BY timestamp DESC LIMIT :saveLast )");
-        query.bindValue(":feed_id", feed->getDbId());
-        query.bindValue(":feed_id2", feed->getDbId());
-        query.bindValue(":olderThan", olderThan.toMSecsSinceEpoch());
-        query.bindValue(":feed_id3", feed->getDbId());
-        query.bindValue(":saveLast", saveLast);
-
-        if (!query.exec()) {
-            reportSQLError(query, "Unable to remove old feed.");
-            db().rollback();
-
-            return;
-        }
+        db().commit();
     }
 
-    db().commit();
+    // If our delete accomplished anything, let's reclaim lost disk space with SQLite's VACUUM command.
+    // According to the SQLite docs, you can't have an open transaction while vacuuming.
+    if (performVacuum) {
+        QSqlQuery query(db());
+        query.exec("VACUUM");
+    }
+
     emit finished(this);
 }
