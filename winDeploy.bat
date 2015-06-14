17cd13539574156da9ba438e52260662c295c5a8
Eric Gregory <mrericsir@gmail.com>
2015-06-14 16:37:39 -0700

    *sigh* Windows is stupid

--- winDeploy.bat
+++ winDeploy.bat
@@ -1,3 +1,3 @@
-%wix%\bin\heat dir Fang -gg -sfrag -cg FangFilesGroup -dr INSTALLDIR -b Fang -out directory.wxs
-%wix%\bin\candle ../../fang.wxs directory.wxs
-%wix%\bin\light fang.wixobj directory.wixobj -b Fang -o Fang.msi
+"%wix%"\bin\heat dir Fang -gg -sfrag -cg FangFilesGroup -dr INSTALLDIR -b Fang -out directory.wxs
+"%wix%"\bin\candle ../../fang.wxs directory.wxs
+"%wix%"\bin\light fang.wixobj directory.wixobj -b Fang -o Fang.msi
