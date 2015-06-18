92f23d1f6ea2f237cc2ba7370b2c435f0d932a0a
Eric Gregory <mrericsir@gmail.com>
2015-06-18 16:10:29 -0700

    Fix for windows versioning

--- winPrebuild.bat
+++ winPrebuild.bat
@@ -1,3 +1,4 @@
 @echo on
+REM This sets the version in TeamCity
 set /p IN_VERSION=<..\APP_VERSION
-echo "##teamcity[setParameter name='APP_VERSION' value='%IN_VERSION%']"
+echo "##teamcity[setParameter name='env.APP_VERSION' value='%IN_VERSION%']"
