258c5fc4cc9f459d429c04ada57ff78e96ae9a24
MrEricSir <mrericsir@gmail.com>
2026-02-28 20:17:49 -0800

    Clean app startup/shutdown

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,14 @@
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
+    // Only run one Fang at a time, fellas.
+    QSingleInstanceCheck single("FangNewsReader");
+    if (single.isAlreadyRunning()) {
+        qDebug() << "Fang is already running";
+        single.notify();
+        return -1;
+    }
+
     qmlRegisterType<FeedValidator>("Fang", 1, 0, "FeedValidator");
     qmlRegisterType<FangSettings>("Fang", 1, 0, "FangSettings");
     qmlRegisterType<NewsFeedInteractor>("Fang", 1, 0, "NewsFeedInteractor");
@@ -44,47 +52,47 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     app.setApplicationName("Fang");
     app.setApplicationVersion(APP_VERSION);
     app.setWindowIcon(QIcon(":/qml/images/full_icon.png"));
-
-    // Only run one Fang at a time, fellas.
-    QSingleInstanceCheck single("FangNewsReader");
-    if (single.isAlreadyRunning()) {
-        single.notify();
-        return -1;
-    }
     
     int ret = 0;
-    QQmlApplicationEngine engine;
-    engine.setNetworkAccessManagerFactory(new FangQQmlNetworkAccessManagerFactory());
-    QQmlFileSelector selector(&engine); // For platform-specific QML files
 
-    // Set QML file selectors.
-    QStringList selectors;
+    // Wrap our application in a code block to (slightly) simplify cleanup,
+    {
+        QQmlApplicationEngine* engine = new QQmlApplicationEngine();
+        engine->setNetworkAccessManagerFactory(new FangQQmlNetworkAccessManagerFactory());
+        QQmlFileSelector selector(engine); // For platform-specific QML files
+
+        // Set QML file selectors.
+        QStringList selectors;
 
 #ifndef QT_WEBVIEW_WEBENGINE_BACKEND
-    // Add a selector indicating we need Qt's native WebView
-    selectors << "webview";
+        // Add a selector indicating we need Qt's native WebView
+        selectors << "webview";
 #endif // QT_WEBVIEW_WEBENGINE_BACKEND
 
 #if defined(Q_OS_ANDROID) || defined(Q_OS_IOS)
-    selectors << "mobile";
+        selectors << "mobile";
 #endif
 
-    if (selectors.count()) {
-        selector.setExtraSelectors(selectors);
-    }
-    
-    // Use a code block to ensure FangApp is deleted.
-    {
-        FangApp fang(&app, &engine, &single);
+        if (selectors.count()) {
+            selector.setExtraSelectors(selectors);
+        }
+
+        // Init app.
+        FangApp fang(&app, engine, &single);
         fang.init();
 
+        // Run app.
         ret = app.exec();
+
+        // Delete QML layer.
+        delete engine;
     }
-    
+
     // Make sure the database is shut down.
     delete DB::instance();
-    
+
+    // Leak check.
     FangObject::printRemainingObjects();
-    
+
     return ret;
 }
